78cfdce84fd3f51fef6e4565d11178be05cb5a04
John Matthew Kassapian <matthewkassapian4@gmail.com>
2022-10-01 12:44:59 -0400

    V3 javadocs and methods wrote down

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -2,7 +2,59 @@ package gr11review.part2;
 
 public class Utility {
 
-    
+    /**
+    * Given a string, determines if "xyz" appears in the middle of the string
+    * To define middle, we say the number of chars to the left and right of "xyz"
+    * must differ by at most one.
+    * @param String x
+    * @return true or false if xyz is in the middle
+    */
+    public static boolean xyzMiddle(String x){
+        
+    }
 
+
+    /**
+    * Given the name of a file, that contatins a single word on each line,
+    * returns the word that has the highest amount of vowels
+    * @param 
+    * @return String representing the word in the file with the most vowels
+    */
+    public static String vowelCount(){
+
+    }
+
+    /**
+    * Return an array that contains the exact same numbers as a given array
+    * but rearranged so that lal the zeros are grouped at the start of the array
+    * the order of non-zero numbers does not matter
+    * @param int[] x
+    * @return An array with the zero's moved to the front
+    */
+    public static int[] zeroFront(){
+
+    }
+
+    /**
+    * Given n>=0 create an array with the pattern `{1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n}`
+    * (spaces added to show the grouping). The length of the array will be 
+    * 1+2+3..+n which is known to sum to exactly n*(n + 1)/2.
+    * @param int n
+    * @return Array with the pattern `{1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n}`
+    */
+
+    public static int[] seriesUp(int n){
+
+    }
+
+    /**
+    * A method that inverts a 2D array. By inverting the array, it means that each
+    * row of the array is now a column, and each column is now a row.
+    * @param int[][] x
+    * @return inverted 2D array
+    */
+    public static int[][] invert(int[][] arr){
+
+    }
     
 }
