dd8a7cb6c8e11a8eec8c585929b5f1a96204cab7
MarcusLohbihler <75390882+MarcusLohbihler@users.noreply.github.com>
2022-10-04 13:00:27 +0000

    Almost done assignment

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,8 +1,108 @@
 package gr11review.part2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Utility {
+        public static void main(String[] args)throws Exception{
+            //tests for methods
+            String h = longestWord("src/gr11review/part2/words.txt");
+            String g = longestWord("src/gr11review/part2/GameNames.txt");
+            System.out.println(h);
+            System.out.println(g);
+            boolean y = linearIn(new int[]{1, 2, 4},(new int[]{2, 4}));
+            System.out.println(y);
+            int[][] r = reverse(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+            System.out.println(r);
+
+
+                }
+                public static boolean xyBalance(String str){
+                    //check that for every x in a string, there is a y after the x's which makes the string balanced. One y can balance multiple x's.
+                    //find elements in string where x and y appear
+                    int xCheck = str.indexOf("x");
+                    int yCheck = str.indexOf("y");
+                    //if y comes after x ycheck is true, if not then return false
+                    if(yCheck > xCheck){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+
+                public static String longestWord(String filenametxt){
+                    try{
+                        //reads contents in file
+                        BufferedReader file = new BufferedReader(new FileReader(filenametxt));
+
+                        String s;
+                        //longest word starts as null
+                        String longestWord = null;                                
+                        while((s = file.readLine()) != null){
+                            int wordLength = s.length();
+                            if(longestWord == null || wordLength > longestWord.length()){
+                                longestWord = s;
+                            }
+                        }
+                        file.close();
+
+                        return longestWord;
+                    }catch(IOException ex){
+                        return ex.getMessage();
+                        
+                    }
+                }
+
+                public static int[] withoutTen(int[] nums){
+                    for(int i = 0; i < nums.length; i++){
+                        while(nums[i] == 10){
+                            for(int j = i; j < nums.length -1; j++){
+                                nums[j] = nums[j + 1];
+                            }
+                            nums[nums.length -1] = 0;
+                        }
+                    }
+                    return nums;
+                }
+
+                public static boolean linearIn(int[] outer, int[] inner){
+                    int innerCount = 0;
+                    for(int i = 0; i < inner.length; i++){
+                        for(int j = 0; j < outer.length; j++){
+                            if(inner[i] == outer[j]){
+                                innerCount++;
+                                break;
+                            }
+                        }          
+                    }
+                    if(innerCount == inner.length){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+                
+                public static int[][] reverse(int[][] arr){
+                    int switchCountOuter = arr.length/2;
+                    int switchCountInner = arr.length;
+                    for(int i = 0; i < switchCountOuter; i++){
+                        int[] x = arr[i];
+                        arr[i] = arr[arr.length - 1 - i];
+                        arr[arr.length - 1 - i] = x;                         
+                        for(int j = 0; j < switchCountInner - 1; j++){
+                            int[] y = arr[j];
+                            arr[j] = arr[arr.length - 1 - j];
+                            arr[arr.length - 1 - j] = y;  
+                        }                      
+                    }
+
+                    
+                    return arr;
+                }
+            }
+
+            
 
     
 
-    
-}
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -1,10 +1,69 @@
 package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
-import gr11review.part2.Utility;
-
 public class UtilityTest{
-  
+  @Test
+    public void methodTest1(){
+        assertEquals(true, Utility.xyBalance("aaxbby"));
+    }
+
+    @Test
+    public void methodTest2(){
+        assertEquals(false, Utility.xyBalance("yx"));
+    }
+
+    @Test
+    public void methodTest3(){
+        assertEquals(true, Utility.xyBalance("xxxxxgy"));
+    }
+
+    @Test
+    public void FileIOTest1() throws IOException {
+        // System.out.println("This is where my files are");
+        // System.out.println(Arrays.toString(new File(".").list()));
+        
+        assertEquals("consectetur", Utility.longestWord("src/gr11review/part2/words.txt"));
+    }
+
+    @Test
+    public void FileIOTest2(){
+        assertEquals("League of Legends", Utility.longestWord("src/gr11review/part2/GameNames.txt"));
+    }
+
+    @Test
+    public void Array1Test1(){
+    assertArrayEquals(new int[]{4, 6, 1, 0, 0, 0, 0}, Utility.withoutTen(new int[]{10, 4, 10, 10, 6, 1, 10}));
+    }
+
+    @Test
+    public void Array1Test2(){
+    assertArrayEquals(new int[]{5, 34, 16, 9, 0, 0}, Utility.withoutTen(new int[]{5, 10, 34, 16, 10, 9}));
+    }
+
+    @Test
+    public void Array2Test1(){
+    assertEquals(true, Utility.linearIn(new int[]{1, 2, 4, 6}, new int[]{2, 4}));
+    }
+
+    @Test
+    public void Array2Test2(){
+    assertEquals(false, Utility.linearIn(new int[]{3, 5}, new int[]{3, 5, 20}));
+    }
+
+    @Test
+    public void Array2Test3(){
+    assertEquals(true, Utility.linearIn(new int[]{10, 6, 33, 59, 4, 7, 16}, new int[]{4, 7}));
+    }
+
+    @Test
+    public void TwoDArrayTest1(){
+    assertArrayEquals(new int[][]{{9, 8, 7},{6, 5, 4},{3,2,1}}, Utility.reverse(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+    }
 }
